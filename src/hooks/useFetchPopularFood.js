0768e80ccdabe8d2fbbc9a4136c805ffaf505e8d
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-22 17:27:41 +0700

    add : hooks to fetch popular food data

---
+++ src/hooks/useFetchPopularFood.js
@@ -0,0 +1,21 @@
+import { useQuery } from "react-query";
+
+const useFetchPopularFood = (pageNumber, pageSize) => {
+  const API_KEY = "38b721c685934201a747226e4d5f4cb9";
+
+  const fetchPopularFood = async () => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&sort=popularity&sortDirection=desc&number=${pageSize}&offset=${
+        (pageNumber - 1) * pageSize
+      }`
+    );
+
+    const data = await response.json();
+    return data.results;
+  };
+  return useQuery(["popularFood", pageNumber], fetchPopularFood, {
+    refetchOnWindowFocus: false,
+  });
+};
+
+export default useFetchPopularFood;
