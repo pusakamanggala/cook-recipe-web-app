65d362d309d98f09e90889457b2c0d0c42dbecb4
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-22 18:03:39 +0700

    update : web icon, desc, and name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,13 @@ const NavBar = () => {
   return (
     <nav className="bg-gray-900/60 backdrop-blur-sm text-white py-4 fixed w-full z-50 px-6">
       <div className="container mx-auto flex items-center  justify-between flex-wrap">
+        {/* Website Name */}
         <div className="flex items-center flex-shrink-0 mr-6">
           <a href="/" className="text-2xl font-bold tracking-wide">
             Recipefy
           </a>
         </div>
+        {/* Navbar Menu */}
         <div className="md:flex hidden">
           <a href="/" className="mr-6">
             Home
@@ -20,6 +22,7 @@ const NavBar = () => {
             About
           </a>
         </div>
+        {/* End of Navbar Menu */}
       </div>
     </nav>
   );
