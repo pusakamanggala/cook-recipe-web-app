94d06644a0e56894a9b5462fd756a58e5e3423d1
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-23 13:30:54 +0700

    delete : navbar and footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,14 @@
 import React from "react";
 import MealType from "../components/MealType";
 import HeroSection from "../components/HeroSection";
-import NavBar from "../components/NavBar";
 import PopularFoods from "../components/PopularFoods";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
-      <NavBar />
       <HeroSection />
       <MealType />
       <PopularFoods />
-      <Footer />
     </>
   );
 };
