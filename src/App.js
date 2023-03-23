b0ed306f0ff861b765af2eaa83167edbd2939755
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-23 13:29:55 +0700

    add : navbar and footer to every page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Home from "./pages/Home";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Recipe from "./pages/Recipe";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -9,9 +12,12 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
         </Routes>
+        <Footer />
       </Router>
     </QueryClientProvider>
   );
