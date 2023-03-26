7cc0d9c931b61bdff85f17edc2ff8a9c4ba00a9f
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-26 15:18:29 +0700

    add : link to navigate

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-900/60 backdrop-blur-sm text-white py-4 fixed w-full z-50 px-6">
+    <nav className="bg-gray-900/60 backdrop-blur-sm text-white py-4 fixed w-full z-40 px-6">
       <div className="container mx-auto flex items-center  justify-between flex-wrap">
         {/* Website Name */}
         <div className="flex items-center flex-shrink-0 mr-6">
@@ -15,9 +16,9 @@ const NavBar = () => {
           <a href="/" className="mr-6">
             Home
           </a>
-          <a href="/recipes" className="mr-6">
+          <Link to={`/search/random/all`} className="mr-6">
             Recipes
-          </a>
+          </Link>
           <a href="/about" className="mr-6">
             About
           </a>
