3bd0c5dff008d68c4aa5308646fa26cd17622b2a
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-26 15:24:56 +0700

    add : navbar and footer to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,17 @@ import React from "react";
 import MealType from "../components/MealType";
 import HeroSection from "../components/HeroSection";
 import PopularFoods from "../components/PopularFoods";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       <HeroSection />
       <MealType />
       <PopularFoods />
+      <Footer />
     </>
   );
 };
