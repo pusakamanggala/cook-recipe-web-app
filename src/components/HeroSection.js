328efde67ea2c317c6b1f870759532d025b2d6cc
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-30 16:14:29 +0700

    update : wave bg placement

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -50,19 +50,6 @@ const HeroSection = () => {
         </div>
       </form>
       {/* End of Search Bar */}
-      {/* Wave Background */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-        className="absolute md:-bottom-8 bottom-0 md:block hidden"
-      >
-        <path
-          fill=" #e5e7eb"
-          fillOpacity="1"
-          d="M0,192L80,208C160,224,320,256,480,250.7C640,245,800,203,960,181.3C1120,160,1280,160,1360,160L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-        ></path>
-      </svg>
-      {/* End of Wave Background */}
     </div>
   );
 };
